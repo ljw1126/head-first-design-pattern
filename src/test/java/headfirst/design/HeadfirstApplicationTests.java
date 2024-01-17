44e60a1ad73d6d28ad7a5527c64bcf545b674f30
이진우 <leejinwoo1126@gmail.com>
2024-01-17 12:48:23 +0900

    init

---
+++ src/test/java/headfirst/design/HeadfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package headfirst.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
