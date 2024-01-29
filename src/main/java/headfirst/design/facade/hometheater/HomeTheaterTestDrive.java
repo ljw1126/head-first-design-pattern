952bb0ada5a8759f7109f83c789aa75b57b21c8f
leejinwoo <leejinwoo1126@gmail.com>
2024-01-29 21:36:33 +0900

    feat: 퍼사드 패턴 예제 학습

---
+++ src/main/java/headfirst/design/facade/hometheater/HomeTheaterTestDrive.java
@@ -0,0 +1,20 @@
+package headfirst.design.facade.hometheater;
+
+public class HomeTheaterTestDrive {
+    public static void main(String[] args) {
+        Amplifier amp = new Amplifier("Amplifier");
+        Tuner tuner = new Tuner("AM/FM Tuner", amp);
+        StreamingPlayer player = new StreamingPlayer("Streaming Player", amp);
+        Projector projector = new Projector("Projector", player);
+        TheaterLights lights = new TheaterLights("Theater Ceiling Lights");
+        Screen screen = new Screen("Theater Screen");
+        PopcornPopper popper = new PopcornPopper("Popcorn Popper");
+
+        HomeTheaterFacade homeTheater =
+                new HomeTheaterFacade(amp, tuner, player, projector, lights, screen, popper);
+
+        homeTheater.watchMovie("Harry Potter");
+        System.out.println("========================");
+        homeTheater.endMovie();
+    }
+}
